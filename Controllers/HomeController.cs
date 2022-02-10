d1c9e675ff1b4ac53d8bea5630c78a17825730bc
ShengLei123 <starkray666666@gmail.com>
2022-02-09 17:19:09 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TaskContext BlahContext { get; set; }
+
+        public HomeController(TaskContext daContext)
+        {
+            BlahContext = daContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            ViewBag.Categories = BlahContext.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(TaskResponse add)
+        {
+            if (ModelState.IsValid)
+            {
+                BlahContext.Add(add);
+                BlahContext.SaveChanges();
+                return View("Confirmation", add);
+            }
+            else
+            {
+                ViewBag.Categories = BlahContext.Categories.ToList();
+                return View(add);
+            }
+            
+        }
+        public IActionResult ViewTasks()
+        {
+            var applications = BlahContext.Responses
+                    .Include(x => x.Category)
+                    .ToList();
+
+            return View(applications);
+        }
+        [HttpGet]
+        public IActionResult Edit(int taskid)
+        {
+            ViewBag.Categories = BlahContext.Categories.ToList();
+            var application = BlahContext.Responses.Single(x => x.TaskId == taskid);
+            return View("Form", application);
+        }
+        [HttpPost]
+        public IActionResult Edit(TaskResponse tr)
+        {
+            BlahContext.Update(tr);
+            BlahContext.SaveChanges();
+            return RedirectToAction("ViewTasks");
+        }
+        [HttpGet]
+        public IActionResult Delete(int taskid)
+        {
+            var application = BlahContext.Responses.Single(x => x.TaskId == taskid);
+            return View("Delete", application);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskResponse tr)
+        {
+            BlahContext.Responses.Remove(tr);
+            BlahContext.SaveChanges();
+
+            return RedirectToAction("ViewTasks");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TaskContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class TaskContext: DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options): base(options)
+        {
+            
+        }
+        public DbSet<TaskResponse> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId=1, CategoryName = "Home"},
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+            );
+            mb.Entity<TaskResponse>().HasData(
+                new TaskResponse
+                {
+                    CategoryId = 1,
+                    TaskId = 1,
+                    Task = "walk dog",
+                    DueDate = DateTime.Parse("01/01/2022"),
+                    Quadrant = "I",
+                    Completed = true
+                },
+                new TaskResponse
+                {
+                    CategoryId = 2,
+                    TaskId = 2,
+                    Task = "breakfast",
+                    DueDate = DateTime.Parse("01/11/2022"),
+                    Quadrant = "II",
+                    Completed = true
+                },
+                new TaskResponse
+                {
+                    CategoryId = 3,
+                    TaskId = 3,
+                    Task = "Eat dinner",
+                    DueDate = DateTime.Parse("01/15/2022"),
+                    Quadrant = "III",
+                    Completed = false
+                }
+            );
+        }
+    }
+}
